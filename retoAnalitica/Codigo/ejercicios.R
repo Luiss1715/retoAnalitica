9a8c0450a0f0613936c561a4b9fe3182d6487dcf
A01747396 <99996482+A01747396@users.noreply.github.com>
2024-05-06 14:11:20 -0600

    Update ejercicios.R

--- retoAnalitica/Codigo/ejercicios.R
+++ retoAnalitica/Codigo/ejercicios.R
@@ -11,7 +11,12 @@ cat("la suma es: ",suma,"\n\n")
 
 
 # 2 -----------------------------------------------------------------------
-
+#Programa que calcula el área de un cuadrado
+lado <- 5 
+#Calculo
+area_cuadrado= lado * lado
+#Resultado
+sprintf("El area del cuadrado es: %f", area)
 
 # 3 -----------------------------------------------------------------------
 #programa que calcula el area de un triangulo
@@ -27,7 +32,12 @@ cat("El área del triángulo es:", area,"\n\n")
 
 
 # 4 -----------------------------------------------------------------------
-
+#Programa que calcula el área de un círculo
+radio <- 4.5
+# Calculo 
+area_circulo= pi*4.5**2
+#Resultado
+sprintf("El area del circulo es: %.2f", area_circulo)
 
 # 5 -----------------------------------------------------------------------
 radio <- 20
@@ -40,7 +50,12 @@ volumen <- (1/3) * pi * radio^2 * altura
 cat("El volumen del cono es:", volumen,"\n\n")
 
 # 6 -----------------------------------------------------------------------
-
+#Programa que convierte pesos mexicanos a USD
+peso = 450
+#Calculo
+cambio= dolar / 20
+#Resultado
+sprintf("El cambio de pesos a dolares : $%.2f", cambio)
 
 # 7 -----------------------------------------------------------------------
 fahrenheit <- 89
