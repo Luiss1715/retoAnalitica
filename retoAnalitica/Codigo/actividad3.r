c55906f8ad47448938fcb4155aff695799e4b532
A01747396 <99996482+A01747396@users.noreply.github.com>
2024-05-07 13:45:25 -0600

    Update actividad3.r

--- retoAnalitica/Codigo/actividad3.r
+++ retoAnalitica/Codigo/actividad3.r
@@ -1,18 +1,25 @@
-seguidores <- c(325, 
-519,
-37,
-0,
-182,
-190,
-235,
-547,
-651,
-124,
-436,
-325,
-648)
+# Actividad 3  Vectores 
 
+#Vector de seguidores 
+seguidores <- c(325,519, 37, 0, 182, 190, 235, 547, 651, 124, 436, 325, 648)
 
-boxplot(seguidores,col ="green")
+sort(seguidores)
+
+# Media 
+mean(seguidores)
+
+#Mediana 
+median(seguidores)
+
+#Varianza 
+var(seguidores)
+
+#Standar deviation 
+sd(seguidores)
+
+plot(seguidores)
+hist(seguidores, col="pink")
+summary(seguidores)
+boxplot(seguidores)
 
 
