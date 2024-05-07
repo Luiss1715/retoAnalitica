5b8518e1711b958e12b3fc878b867d53dc2f7ff8
Luiss1715 <A01751150@tec.mx>
2024-05-07 15:10:24 -0600

    Update actividad4.R

--- retoAnalitica/Codigo/actividad4.R
+++ retoAnalitica/Codigo/actividad4.R
@@ -1,35 +1,57 @@
 
 getwd()
-
 data <- read.csv("./GlobalLandTemperaturesByCountry.csv")
-
-
 #limpiamos data
-
 newData <- data[!is.na(data$AverageTemperature) & !is.na(data$AverageTemperatureUncertainty), ]
-#newData <- data[!is.na(data$AverageTemperatureUncertainty),]
-#View(newData)
 
-#sapply(newData,function(x)sum(is.na(x)))
 
-#una vez limipio, hacemos el analisis de temperatura promedio por
-#cada pais por cada año 
-
-# Supongamos que tu dataframe se llama 'data' y tiene las columnas: fecha, temperatura, temperaturaIncertidumbre y pais
 library(ggplot2)
 
-# Primero, agrupamos los datos por país y calculamos la temperatura promedio para cada país
 avg_temp_by_country <- aggregate(AverageTemperature ~ Country, data = newData, FUN = mean, na.rm = TRUE)
 
-paises_a_graficar <- c("Slovenia", "Tanzania", "Turkey")  # Reemplaza 'Pais1', 'Pais2' y 'Pais3' con los nombres de los países que desees graficar
+paises_a_graficar <- c("Slovenia", "Tanzania", "Turkey")
 
-avg_temp_by_country_filtrado <- avg_temp_by_country[avg_temp_by_country$AverageTemperature %in% paises_a_graficar, ]
+
+avg_temp_by_country_filtrado <- subset(avg_temp_by_country, Country %in% paises_a_graficar)
 
 # Ahora creamos el gráfico de barras
 ggplot(avg_temp_by_country_filtrado, aes(x = Country, y = AverageTemperature)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   labs(title = "Temperatura Promedio por País",
        x = "País",
+       y = "Temperatura Promedio") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+#grafica del año 2013
+data_2013 <- subset(newData, substr(dt, 1, 4) == "2013" & Country %in% c("Slovenia", "Tanzania", "Turkey"))
+
+# Ahora agrupamos los datos por país y calculamos la temperatura promedio para cada país
+avg_temp_by_country_2013 <- aggregate(AverageTemperature ~ Country, data = data_2013, FUN = mean, na.rm = TRUE)
+
+# Ahora creamos el gráfico de barras
+ggplot(avg_temp_by_country_2013, aes(x = Country, y = AverageTemperature)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(title = "Temperatura Promedio por País en 2013",
+       x = "País",
+       y = "Temperatura Promedio") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+
+
+#graficar todos los años
+data_selected_countries <- subset(newData, Country %in% c("Slovenia", "Tanzania", "Turkey"))
+data_selected_countries$Year <- format(as.Date(data_selected_countries$dt), "%Y")
+data_selected_countries$Year <- as.numeric(data_selected_countries$Year)
+avg_temp_by_year <- aggregate(AverageTemperature ~ Country + Year, data = data_selected_countries, FUN = mean, na.rm = TRUE)
+ggplot(avg_temp_by_year, aes(x = Year, y = AverageTemperature, color = Country)) +
+  geom_line(color = "red") +
+  facet_wrap(~ Country, scales = "free_y") +
+  labs(title = "Temperatura Promedio por Año",
+       x = "Año",
        y = "Temperatura Promedio") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
