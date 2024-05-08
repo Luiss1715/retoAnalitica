079bf80bc258817c3b33bb0ebf799af76e520923
A01747396 <99996482+A01747396@users.noreply.github.com>
2024-05-08 14:29:09 -0600

    Update actividad5.R

--- retoAnalitica/Codigo/actividad5.R
+++ retoAnalitica/Codigo/actividad5.R
@@ -19,3 +19,29 @@ fviz_cluster(kM,mtcars)
 carsEscalado <- scale(as.matrix(mtcars))
 kM2 <- kmeans(carsEscalado,4)
 fviz_cluster(kM2,mtcars)
+
+
+# Graficos de correlacion 
+#Correlación entre millas por galón y desplazamiento
+cor(mtcars$mpg, mtcars$disp)
+corGE <- data.frame(mtcars$mpg, mtcars$disp)
+chart.Correlation(corGE)
+
+
+#Correlación entre millas por galón y caballos
+cor(mtcars$mpg, mtcars$hp)
+corGE <- data.frame(mtcars$mpg, mtcars$hp)
+chart.Correlation(corGE)
+
+
+#Correlación entre millas por galón y relación del eje trasero
+cor(mtcars$mpg, mtcars$drat)
+corGE <- data.frame(mtcars$mpg, mtcars$drat)
+chart.Correlation(corGE)
+
+
+
+#Correlación entre millas por galón y peso
+cor(mtcars$mpg, mtcars$wt)
+corGE <- data.frame(mtcars$mpg, mtcars$wt)
+chart.Correlation(corGE)
