b3c4781971f9b1405582eaff222043c059287086
A01747396 <99996482+A01747396@users.noreply.github.com>
2024-05-09 15:05:01 -0600

    Update Reto.R

--- retoAnalitica/Codigo/Reto.R
+++ retoAnalitica/Codigo/Reto.R
@@ -72,3 +72,76 @@ HurEscalado <- scale(as.matrix(dataCorrelation))
 kM2 <- kmeans(HurEscalado,2)
 fviz_cluster(kM2,dataCorrelation)
 
+
+
+#Cuartil del viento 
+summary(data$Wind)
+boxplot(data$Wind, main= "Cuartiles del viento")
+
+
+
+
+#Cuartil de la presión 
+summary(data$Pressure)
+boxplot(data$Pressure, main= "Cuartiles de la presión")
+
+
+#Cuartil del CO2 
+summary(data$CO2)
+boxplot(data$CO2, main= "Cuartiles del CO2")
+
+#Cuartil de la población 
+summary(data$Population)
+boxplot(data$Population, main="Cuartiles de la población")
+
+
+#Graficar la variable WIND 
+plot(data$Wind, main= "Gráfica del viento", col= "blue")
+
+data <- data[order(data$year), ]
+plot(data$year, data$Wind, 
+     main = "Año vs. Viento", 
+     xlab = "Año", 
+     ylab = "Viento",
+     col = "blue") 
+
+# Año vs presión 
+plot(data$year, data$Pressure, 
+     main = "Año vs. Presión", 
+     xlab = "Año", 
+     ylab = "Presión",
+     col = "red") 
+
+#Correlación entre presión y viento
+cor(data$Wind, data$Pressure)
+corGE <- data.frame(data$Wind, data$Pressure)
+chart.Correlation(corGE, main= "Viento vs. Presión", col="red", lwd=15)
+
+# Población vs C02 
+plot(data$Population, data$CO2, 
+     main = "Población vs. CO2", 
+     xlab = "Población", 
+     ylab = "CO2",
+     col = "orange") 
+
+
+# Lugares donde se producen más huracanes 
+#Para visualizar la gráfica, se volvió a colocar la columna de océano
+ggplot(data, aes(x = Ocean, fill = Ocean)) +
+  geom_bar() +
+  labs(title = "Frecuencia de huracanes por océano") +
+  scale_fill_manual(values = c("pink", "violet")) 
+
+#Temporada de huracanes
+#Para visualizar la gráfica, se volvió a colocar la columna de mes. 
+
+data$Month <- factor(data$Month, levels = c("January", "February", "March", "April", "May", "June", 
+                                            "July", "August", "September", "October", "November", "December"))
+ggplot(data, aes(x = Month)) +
+  geom_bar() +
+  labs(title = "Frecuencia de observaciones por mes")
+
+
+
+
+#
